d51d093605e0426c1f192f6728e7acae0c3f577c
Brisk4t <atharvapcat@gmail.com>
2023-02-12 01:33:49 -0700

    Added error handling based on exception subclass

--- ongaku.py
+++ ongaku.py
@@ -7,10 +7,28 @@ from discord.ext import commands
 load_dotenv()
 intents = discord.Intents.all()
 command_channel = int(os.getenv('COMMAND_CHANNEL'))
+testing_channel = int(os.getenv('TESTING_CHANNEL'))
 intents.message_content = True
 TOKEN = os.getenv('DISCORD_TOKEN') # Get token from .env
 bot = commands.Bot(command_prefix="!", intents=intents) # Discord interaction object & load default intents
 
+
+
+#classes
+
+class invalidchannel(commands.CheckFailure):
+    pass
+
+
+#checks
+
+
+def check_channel():
+    def predicate(ctx):
+        if not (ctx.channel.id == command_channel or ctx.channel.id == testing_channel):
+            raise invalidchannel("Invalid Channel")
+        return True
+    return commands.check(predicate)
 
 
 
@@ -24,11 +42,20 @@ async def on_ready(): # On connect
 
 
 @bot.command()
+@check_channel()
 async def test(ctx): # if !test is sent
-    if await check_channel(ctx):
-        await ctx.send("Test Success", delete_after=5)
+    await ctx.send("Test Success", delete_after=5)
 
 
+@bot.command()
+async def horny(ctx):
+    await ctx.send("Chup lavde krish saala paani hai nahi horny bol raha.")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, invalidchannel):
+        await ctx.send("Please use the Ongaku Commands channel for bot commands.")
 
 
 
@@ -36,13 +63,13 @@ async def test(ctx): # if !test is sent
 
 #helper functions
 
-async def check_channel(ctx):
-    if ctx.channel.id == command_channel:
-        return True
+# async def check_channel(ctx):
+#     if ctx.channel.id == command_channel:
+#         return True
     
-    else:
-        await ctx.send("Please use the Ongaku Commands channel for bot commands", delete_after=5)
-        return False
+#     else:
+#         await ctx.send("Please use the Ongaku Commands channel for bot commands", delete_after=5)
+#         return False
 
 
 bot.run(TOKEN)
