74ae544fbdeb08936975f535a2aa2c3ff89d400c
Brisk4t <atharvapcat@gmail.com>
2023-02-11 22:52:21 -0700

    Added !test and command channel verification

--- ongaku.py
+++ ongaku.py
@@ -1,17 +1,48 @@
 import os
 import discord
 from dotenv import load_dotenv
-
+from discord.ext import commands
 
 # Initialize discord API objects
-intents = discord.Intents.default()
-intents.message_content = True
 load_dotenv()
+intents = discord.Intents.all()
+command_channel = int(os.getenv('COMMAND_CHANNEL'))
+intents.message_content = True
 TOKEN = os.getenv('DISCORD_TOKEN') # Get token from .env
-client = discord.Client(intents=intents) # Discord interaction object & load default intents
+bot = commands.Bot(command_prefix="!", intents=intents) # Discord interaction object & load default intents
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to discord')
 
-client.run(TOKEN)
+
+
+# Event Handlers
+
+@bot.event
+async def on_ready(): # On connect 
+    print(f'{bot.user} has connected to discord')
+    channel = bot.get_channel(command_channel)
+    await channel.send("Ongaku Online.", delete_after=10)
+
+
+@bot.command()
+async def test(ctx): # if !test is sent
+    if await check_channel(ctx):
+        await ctx.send("Test Success", delete_after=5)
+
+
+
+
+
+
+
+#helper functions
+
+async def check_channel(ctx):
+    if ctx.channel.id == command_channel:
+        return True
+    
+    else:
+        await ctx.send("Please use the Ongaku Commands channel for bot commands", delete_after=5)
+        return False
+
+
+bot.run(TOKEN)
